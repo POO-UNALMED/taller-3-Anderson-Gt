f0f5f5abd292e6b40080778ad0db751f64acfb50
Anderson Gutierrez <69688234+Anderson-Gt@users.noreply.github.com>
2020-09-03 16:36:16 -0500

    Add files via upload

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,18 @@
+package taller3.televisores;
+
+class Marca {
+	private String nombre;
+	
+	Marca(String nombre){
+		this.nombre = nombre;				
+	}
+	
+	public String getNombre () {
+		return this.nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
